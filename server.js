ef184b055b03e7a3caea17e521de56d73220e26f
Kadhir Patchamuthu <kadhirp@ad.unc.edu>
2022-03-23 18:44:00 -0400

    Added coinFlip endpoint

--- server.js
+++ server.js
@@ -1,21 +1,32 @@
 const express = require('express')
 const app = express()
-
+const args = require("minimist")(process.argv.slice(2))
 //Command Line Argument for port here
-var port = 5000
+args["port"]
+const port = args.port || process.env.PORT || 3000
 
 const server = app.listen(port, () => {
     console.log(`Server is running on ${port}`)
     //Same asconsole.log('App is running on port %PORT%'.replace('$PORT$', port))
 })
 
+
+function coinFlip() {
+    return (Math.floor(Math.random() * 2) == 0 ) ? 'heads' : 'tails';
+  }
+
 app.get('/app', (req, res) => {
     res.status(200).end('The bugger works')
     res.type("text/plain")
 })
 
+app.get('/app/flip', (req, res) => {
+    var flip = coinFlip()
+    res.status(200).json({'flip': flip})
+})
+
 app.use(function(req, res) {
-    res.status(404).send("Endpoint does not exist")
+    res.status(404).send("404 NOT FOUND")
     res.type("text/plain")
 }
 )
