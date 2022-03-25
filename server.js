2596772ee4cceb224bc74a127749e9fa68694967
Kadhir Patchamuthu <kadhirp@ad.unc.edu>
2022-03-25 17:04:40 -0400

    Edited endpoint types

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const args = require("minimist")(process.argv.slice(2))
 //Command Line Argument for port here
 args["port"]
-const port = args.port || process.env.PORT || 3000
+const port = args.port || process.env.PORT || 5000
 
 const server = app.listen(port, () => {
     console.log(`Server is running on ${port}`)
@@ -60,19 +60,20 @@ function coinFlip() {
 
 app.get('/app', (req, res) => {
     res.status(200).end('OK')
-    res.type("text/plain")
+    res.type("text/json")
 })
 
 app.get('/app/flip', (req, res) => {
     var flip = coinFlip()
     res.status(200).json({'flip': flip})
-    res.type("text/join")
+    res.type("text/json")
 })
 
 app.get('/app/flips/', (req, res) => {
     var flips = coinFlips(1)
     var count = countFlips(flips)
     res.status(200).json( {'raw': flips, 'summary':count})
+    res.type("text/json")
 })
 
 app.get('/app/flips/:number', (req, res) => {
@@ -86,10 +87,12 @@ app.get('/app/call/:guess', (req, res) => {
     var flip = coinFlip()
     if (req.params.guess != 'heads' && req.params.guess != 'tails'){
         res.status(404).end('Invalid Input')
+        res.type("text/json")
     }
     else{
         var result = flip == req.params.guess ? 'win':'lose'
         res.status(200).json({'call': req.params.guess, 'flip': flip, 'result':result })
+        res.type("text/json")
     }
     
 })
