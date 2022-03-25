68d032f1b6273c55686cd45765e89b15173e771d
Kadhir Patchamuthu <kadhirp@ad.unc.edu>
2022-03-25 16:28:53 -0400

    Added flips with parameter and without parameter

--- server.js
+++ server.js
@@ -15,6 +15,36 @@ function coinFlip() {
     return (Math.floor(Math.random() * 2) == 0 ) ? 'heads' : 'tails';
   }
 
+  function coinFlips(flips) {
+    var arr = []
+    if (flips == null || flips == 0){
+      arr[0] = coinFlip();
+      return arr
+    }
+    for (let i=0;i <flips; i++){
+      arr[i] = coinFlip();
+    }
+    return arr
+ }
+
+ function countFlips(array) {
+    var xCounter = 0;
+    var yCounter = 0;
+    for (var i=0; i<array.length; i++){
+      array[i] == 'heads' ? xCounter++:yCounter++;
+    }
+    if (xCounter == 0){
+      return {tails: yCounter}
+    }
+    else if (yCounter == 0){
+      return {heads: xCounter}
+    }
+    else{
+      return {heads: xCounter, tails: yCounter}
+    }
+    
+  }
+
 app.get('/app', (req, res) => {
     res.status(200).end('OK')
     res.type("text/plain")
@@ -23,10 +53,26 @@ app.get('/app', (req, res) => {
 app.get('/app/flip', (req, res) => {
     var flip = coinFlip()
     res.status(200).json({'flip': flip})
+    res.type("text/join")
 })
+
+app.get('/app/flips/', (req, res) => {
+    var flips = coinFlips(1)
+    var count = countFlips(flips)
+    res.status(200).json( {'raw': flips, 'summary':count})
+})
+
+app.get('/app/flips/:number', (req, res) => {
+    var flips = coinFlips(req.params.number)
+    var count = countFlips(flips)
+    res.status(200).json( {'raw': flips, 'summary':count})
+})
+
+
 
 app.use(function(req, res) {
     res.status(404).send("404 NOT FOUND")
     res.type("text/plain")
 }
+
 )
